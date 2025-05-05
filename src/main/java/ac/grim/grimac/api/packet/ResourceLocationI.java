e378b866d7a8f16e4db62df1f7c8005ae29dc1de
Axionize <154778082+Axionize@users.noreply.github.com>
2025-05-05 16:10:25 -0400

    Add ResourceLocation interface to add or injected into platform impls

---
+++ src/main/java/ac/grim/grimac/api/packet/ResourceLocationI.java
@@ -0,0 +1,4 @@
+package ac.grim.grimac.api.packet;
+
+public interface ResourceLocationI {
+}
--- src/main/java/ac/grim/grimac/api/packet/item/PacketItemType.java
+++ src/main/java/ac/grim/grimac/api/packet/item/PacketItemType.java
@@ -1,6 +1,7 @@
 package ac.grim.grimac.api.packet.item;
 
 import ac.grim.grimac.api.GrimAPIProvider;
+import ac.grim.grimac.api.packet.ResourceLocationI;
 import ac.grim.grimac.api.packet.manager.PacketItemTypeManager;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -24,7 +25,7 @@ public interface PacketItemType {
     boolean hasAttribute(PacketItemAttribute itemAttribute);
 
     // Remember to make sure the Optimzation in PacketEventsInit for loading name works
-    void getName();
+    ResourceLocationI getName();
 
     PacketStateType getPlacedType();
 
